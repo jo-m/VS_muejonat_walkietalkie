a44bd99cbaff8a6457c4bc3efada1c2b6b206532
Jonathan Mueller <j.mueller@apoveda.ch>
2014-11-24 17:31:26 +0100

    autodiscovery works

--- src/com/example/testwifi/MainActivity.java
+++ src/com/example/testwifi/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.testwifi;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
+import android.net.wifi.p2p.WifiP2pManager.DnsSdServiceResponseListener;
+import android.net.wifi.p2p.WifiP2pManager.DnsSdTxtRecordListener;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceInfo;
+import android.net.wifi.p2p.nsd.WifiP2pDnsSdServiceRequest;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -19,9 +30,35 @@ public class MainActivity extends Activity {
 	BroadcastReceiver mReceiver;
 	IntentFilter mIntentFilter;
 	TextView mTextView;
+	WifiP2pDnsSdServiceInfo mServiceInfo;
+	WifiP2pDnsSdServiceRequest mServiceRequest;
+	
+	private final String LOGTAG = "WIFI_P2P_VS2";
+	private final String SERVICE_NAME = "_walkietalkie._tcp";
+	
+	private final int SERVER_PORT = 42634;
 	
 	public TextView getTextView() {
 		return mTextView;
+	}
+	
+	private void setupService() {
+		WifiManager wifiMan = (WifiManager) this.getSystemService(
+                Context.WIFI_SERVICE);
+       	WifiInfo wifiInf = wifiMan.getConnectionInfo();
+       	String macAddr = wifiInf.getMacAddress();
+       	
+		//  Create a string map containing information about your service.
+        Map<String, String> record = new HashMap<String, String>();
+        record.put("listenport", String.valueOf(SERVER_PORT));
+        record.put("mac", macAddr);
+        record.put("name", "name=" + macAddr);
+
+        // Service information.  Pass it an instance name, service type
+        // _protocol._transportlayer , and the map containing
+        // information other devices will want once they connect to this one.
+        mServiceInfo =
+                WifiP2pDnsSdServiceInfo.newInstance(macAddr, SERVICE_NAME, record);
 	}
 	
 	@Override
@@ -39,19 +76,27 @@ public class MainActivity extends Activity {
 	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION);
 	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
 	    mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
+	    
+	    setupService();
+	    setupServiceListeners();
 	}
 	
-	/* register the broadcast receiver with the intent values to be matched */
 	@Override
 	protected void onResume() {
 	    super.onResume();
 	    registerReceiver(mReceiver, mIntentFilter);
+	    mManager.addLocalService(mChannel, mServiceInfo, null);
+	    
+	    mServiceRequest = WifiP2pDnsSdServiceRequest.newInstance();
+        mManager.addServiceRequest(mChannel, mServiceRequest, null);
 	}
-	/* unregister the broadcast receiver */
+	
 	@Override
 	protected void onPause() {
 	    super.onPause();
 	    unregisterReceiver(mReceiver);
+	    mManager.removeLocalService(mChannel, mServiceInfo, null);
+	    mManager.removeServiceRequest(mChannel, mServiceRequest, null);
 	}
 
 	@Override
@@ -68,15 +113,65 @@ public class MainActivity extends Activity {
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_scan) {
-			mManager.discoverPeers(mChannel, new WifiP2pManager.ActionListener() {
-		        @Override
-		        public void onSuccess() {}
-
-		        @Override
-		        public void onFailure(int reasonCode) {}
-		    });
+			mManager.discoverServices(mChannel, null);
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	public class Buddy {
+		String name;
+		String mac;
+		String ip;
+		int serverPort;
+		boolean rightService = false;
+	}
+	
+	final HashMap<String, Buddy> buddies = new HashMap<String, Buddy>();
+
+	private void setupServiceListeners() {
+	    DnsSdTxtRecordListener txtListener = new DnsSdTxtRecordListener() {
+			@Override
+			public void onDnsSdTxtRecordAvailable(String fullDomain, Map<String, String> record, WifiP2pDevice device) {
+	            Buddy b;
+				if(buddies.containsKey(device.deviceAddress)) {
+					b = buddies.get(device.deviceAddress);
+	            } else {
+	            	b = new Buddy();
+	            }
+	            
+	            b.name = record.get("name");
+	            b.mac = record.get("mac");
+	            b.serverPort = Integer.parseInt(record.get("listenport"));
+	            b.ip = device.toString();
+				
+				if(!buddies.containsKey(device.deviceAddress)) {
+					buddies.put(device.deviceAddress, b);
+	            }
+			}
+	    };
+	    
+	    DnsSdServiceResponseListener servListener = new DnsSdServiceResponseListener() {
+	        @Override
+	        public void onDnsSdServiceAvailable(String instanceName, String registrationType,
+                WifiP2pDevice device) {
+                Buddy b;
+				if(buddies.containsKey(device.deviceAddress)) {
+					b = buddies.get(device.deviceAddress);
+	            } else {
+	            	b = new Buddy();
+	            }
+			
+    			if(registrationType.startsWith(SERVICE_NAME)) {
+	                Toast.makeText(MainActivity.this, "discovered "+b.mac, Toast.LENGTH_LONG).show();
+    			}
+    		
+    			if(!buddies.containsKey(device.deviceAddress)) {
+					buddies.put(device.deviceAddress, b);
+	            }	
+	        }
+	    };
+
+	    mManager.setDnsSdResponseListeners(mChannel, servListener, txtListener);
 	}
 }
--- src/com/example/testwifi/WiFiDirectBroadcastReceiver.java
+++ src/com/example/testwifi/WiFiDirectBroadcastReceiver.java
@@ -14,13 +14,13 @@ import android.widget.Toast;
 /**
  * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
  */
-public class WiFiDirectBroadcastReceiver extends BroadcastReceiver implements PeerListListener {
+public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
     private WifiP2pManager mManager;
     private Channel mChannel;
     private MainActivity mActivity;
     private Context mContext;
-    private final String LOGTAG = "WIFI P2P";
+    private final String LOGTAG = "WIFI_P2P_VS2";
 
     public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel,
     		MainActivity activity) {
@@ -37,37 +37,16 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver implements Pe
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
         	int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-        	Log.d(LOGTAG, "SEARCH FOR PEERS");
+
             if (state != WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
             	Toast.makeText(context, "You do not have WiFi P2P available!", Toast.LENGTH_LONG).show();
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
-        	Toast.makeText(context, "discover peers", Toast.LENGTH_LONG).show();
-        	
-        	if (mManager != null) {
-                mManager.requestPeers(mChannel, this);
-                
-                Log.d(LOGTAG, "SEARCH FOR PEERS");
-            }
+
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
             // Respond to new connection or disconnections
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
             // Respond to this device's wifi state changing
         }
     }
-
-	@Override
-	public void onPeersAvailable(WifiP2pDeviceList peers) {
-		Toast.makeText(mContext, "got peers", Toast.LENGTH_LONG).show();
-		
-		Log.d(LOGTAG, "GOT PEERS " + peers.getDeviceList().size());
-		StringBuilder s = new StringBuilder();
-		
-		for(WifiP2pDevice d: peers.getDeviceList()) {
-			Log.d(LOGTAG, "PEER" + d.toString());
-			
-			s.append("Peer" + d.deviceAddress + " " + d.deviceName + " " + d.primaryDeviceType + "\n\n");
-		}
-		mActivity.getTextView().setText(s.toString());
-	}
 }
